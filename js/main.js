406c720a23d5574746319646f6c3038b64ccaa4d
lberthaut <louis.berthaut@gmx.com>
2021-06-10 16:28:44 +0200

    montage dropdown css et js

--- js/main.js
+++ js/main.js
@@ -6,9 +6,26 @@ for(let recipe of recipes){
 
 
 /*Affichage du dropdown Ingredients*/
-var ingredientsDropdown = document.getElementsByClassName('ingredients-list');
-for(let ingredients of recipes){
-    for(let ingredient of ingredients){
-      let ingredientFiltered = new set(ingredient);
-  ingredientsDropdown.innerHTML += ingredientFiltered.show();
-}}
+var ingredientsDropdown = document.querySelector('.ingredients-list');
+for(let recipe of recipes){
+    for(let ingredient of recipe.ingredients){
+      let ingredientValue = Object.values(ingredient);
+      ingredientsDropdown.innerHTML = `<a class="dropdown-item" href="#">${ingredientValue[0]}</a>`
+    }
+}
+
+/*Affichage du dropdown Appareils*/
+var applianceDropdown = document.querySelector('.appliances-list');
+for(let recipe of recipes){
+    let appliance = recipe.appliance;
+      applianceDropdown.innerHTML = `<a class="dropdown-item" href="#">${appliance}</a>`
+    }
+
+/*Affichage du dropdown Ustensiles*/
+var ustensilsDropdown = document.querySelector('.ustensils-list');
+for(let recipe of recipes){
+    for(let ustensils of recipe.ustensils){
+      let ustensilsValue = Object.values(ustensils);
+      ustensilsDropdown.innerHTML = `<a class="dropdown-item" href="#">${ustensilsValue}</a>`
+    }
+}
