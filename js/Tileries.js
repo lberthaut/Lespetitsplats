1bd800c0eb6f522092f579f56340304867d2c59c
lberthaut <louis.berthaut@gmx.com>
2021-06-29 12:10:32 +0200

    montage fermeture tuiles

--- js/Tileries.js
+++ js/Tileries.js
@@ -1,45 +1,50 @@
 const tileriesArea = document.getElementById('tileries-searched');
 
-
-document.querySelectorAll('.ingredient-item').forEach(e => e.addEventListener('click', function (){
+const itemsIngredients = document.querySelectorAll('.ingredient-item');
+itemsIngredients.forEach(e => e.addEventListener('click', function (){
     var value = this.dataset.value;
     this.style.color="grey";
-    this.style.cursor="default";
-    tileriesArea.innerHTML += `<div class="tile-ingredient-searched tile">
-    <p class="tile-value" data-value="${value}">${value}</p>
+    this.style.pointerEvents="none";
+    tileriesArea.innerHTML += `<div class="tile-ingredient-searched tile" data-value="${value}">
+    <p class="tile-value">${value}</p>
     <i class="far fa-times-circle tile-close"></i>
     </div>`
     closeTile();
-    }, {once: true}
+    }
 ))
 
 document.querySelectorAll('.appliance-item').forEach(e => e.addEventListener('click', function (){
     var value = this.dataset.value;
     this.style.color="grey";
-    this.style.cursor="default";
-    tileriesArea.innerHTML += `<div class="tile-appliance-searched tile">
+    this.style.pointerEvents="none";
+    tileriesArea.innerHTML += `<div class="tile-appliance-searched tile" data-value="${value}">
     <p class="tile-value">${value}</p>
     <i class="far fa-times-circle tile-close"></i>
     </div>`
     closeTile();
-    }, {once: true}
+    }
 ))
 
 document.querySelectorAll('.ustensil-item').forEach(e => e.addEventListener('click', function (){
     var value = this.dataset.value;
     this.style.color="grey";
-    this.style.cursor="default";
-    tileriesArea.innerHTML += `<div class="tile-ustensil-searched tile">
+    this.style.pointerEvents="none";
+    tileriesArea.innerHTML += `<div class="tile-ustensil-searched tile" data-value="${value}">
     <p class="tile-value">${value}</p>
     <i class="far fa-times-circle tile-close"></i>
     </div>`
     closeTile();
-    }, {once: true}
+    }
 ))
 
 function closeTile(){
     document.querySelectorAll('.tile-close').forEach(e => e.addEventListener('click', function (){
         this.closest('.tile').classList.add('remove');
+        var tileValue = this.closest('tile').data.value;
+        var itemValue = document.getElementsByClassName('.combo-list').querySelector('p[data-value="${tileValue}"');
+        if(tileValue == itemValue.data.value){
+            itemValue.style.color="white";
+            itemValue.style.pointerEvents="initial";
         }
-    ))
+    }))
 }
