0f58f4e40663238b9f75defe88773e85da443dc4
Louis Berthaut <louis.berthaut@gmx.com>
2021-07-06 16:15:19 +0200

    montage tuiles

--- js/Tileries.js
+++ js/Tileries.js
@@ -1,35 +1,10 @@
 const tileriesArea = document.getElementById('tileries-searched');
 
-const itemsIngredients = document.querySelectorAll('.ingredient-item');
-itemsIngredients.forEach(e => e.addEventListener('click', function (){
-    var value = this.dataset.value;
-    this.style.color="grey";
-    this.style.pointerEvents="none";
-    tileriesArea.innerHTML += `<div class="tile-ingredient-searched tile" data-value="${value}">
-    <p class="tile-value">${value}</p>
-    <i class="far fa-times-circle tile-close"></i>
-    </div>`
-    closeTile();
-    }
-))
-
-document.querySelectorAll('.appliance-item').forEach(e => e.addEventListener('click', function (){
-    var value = this.dataset.value;
-    this.style.color="grey";
-    this.style.pointerEvents="none";
-    tileriesArea.innerHTML += `<div class="tile-appliance-searched tile" data-value="${value}">
-    <p class="tile-value">${value}</p>
-    <i class="far fa-times-circle tile-close"></i>
-    </div>`
-    closeTile();
-    }
-))
-
-document.querySelectorAll('.ustensil-item').forEach(e => e.addEventListener('click', function (){
-    var value = this.dataset.value;
-    this.style.color="grey";
-    this.style.pointerEvents="none";
-    tileriesArea.innerHTML += `<div class="tile-ustensil-searched tile" data-value="${value}">
+document.querySelectorAll('.appliance-item, .ingredient-item, .ustensil-item').forEach(e => e.addEventListener('click', function (){
+    let value = this.dataset.value;
+   this.classList.add('disabled');
+   let combobox = this.closest('.combobox');
+    tileriesArea.innerHTML += `<div class="tile-${combobox.dataset.item}-searched tile" data-item="${combobox.dataset.item}" data-value="${value}">
     <p class="tile-value">${value}</p>
     <i class="far fa-times-circle tile-close"></i>
     </div>`
@@ -39,12 +14,12 @@ document.querySelectorAll('.ustensil-item').forEach(e => e.addEventListener('cli
 
 function closeTile(){
     document.querySelectorAll('.tile-close').forEach(e => e.addEventListener('click', function (){
-        this.closest('.tile').classList.add('remove');
-        var tileValue = this.closest('tile').data.value;
-        var itemValue = document.getElementsByClassName('.combo-list').querySelector('p[data-value="${tileValue}"');
-        if(tileValue == itemValue.data.value){
-            itemValue.style.color="white";
-            itemValue.style.pointerEvents="initial";
+        var tile = this.closest('.tile');
+        var combobox = document.querySelector(`.combobox[data-item="${tile.dataset.item}"]`);
+       let item = combobox.querySelector(`p[data-value="${tile.dataset.value}"]`);
+        if(item != null){
+           item.classList.remove('disabled');
+           tile.remove();
         }
     }))
 }
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /*Affichage des recettes*/
-var recipeElement = document.getElementById('recipes-list');
-for(let recipe of recipes){
+var recipeElement = document.getElementById("recipes-list");
+for (let recipe of recipes) {
   recipeElement.innerHTML += recipe.show();
 }
 
@@ -9,37 +9,59 @@ showIngredients(loadIngredients(recipes));
 showAppliances(loadAppliances(recipes));
 showUstensils(loadUstensils(recipes));
 
-
 /* Rechercher des items dans les combobox */
-document.querySelectorAll('.inputcombobox').forEach(searchbar => {
-  searchbar.addEventListener('input', function () {
-    let show = window['show'+this.dataset.item[0].toUpperCase()+this.dataset.item.slice(1)+'s'];
-    if(this.value.length >= 3){
-      let items = searchItem(this.value, recipes, this.dataset.item);
+document.querySelectorAll(".inputcombobox").forEach((searchbar) => {
+  searchbar.addEventListener("input", function () {
+    let show =
+      window[
+        "show" +
+          this.closest('.combobox').dataset.item[0].toUpperCase() +
+          this.closest('.combobox').dataset.item.slice(1) +
+          "s"
+      ];
+    if (this.value.length >= 3) {
+      let items = searchItem(this.value, recipes, this.closest('.combobox').dataset.item);
       show(items);
-      this.style.width="185px";
-      this.closest('.combobox').querySelector('.combo-list').style.flexDirection="column";
-    } else{
-        this.closest('.combobox').querySelector('.combo-list').style.flexDirection="row";
-        let load = window['load'+this.dataset.item[0].toUpperCase()+this.dataset.item.slice(1)+'s'];
-        show(load(recipes));
-        this.style.width="555px";
+      this.style.width = "185px";
+      this.closest(".combobox").querySelector(
+        ".combo-list"
+      ).style.flexDirection = "column";
+    } else {
+      this.closest(".combobox").querySelector(
+        ".combo-list"
+      ).style.flexDirection = "row";
+      let load =
+        window[
+          "load" +
+            this.closest('.combobox').dataset.item[0].toUpperCase() +
+            this.closest('.combobox').dataset.item.slice(1) +
+            "s"
+        ];
+      show(load(recipes));
+      this.style.width = "555px";
     }
-  })
+  });
 
-/* searchbar.addEventListener('blur', function (){
-    if(this.closest('.combobox').querySelector('.combo-list').addEventListener('click', function(){})){
-    }else{
-      this.closest('.combobox').querySelector('.combo-list').classList.remove('activ');
-      this.style.width="185px";}
-  }) */
 
-  searchbar.addEventListener('focus', function (){
-    this.closest('.combobox').querySelector('.combo-list').classList.add('activ');
-    this.style.width="555px";
-    if(this.value.length >= 3){
-      this.style.width="185px";
+
+  searchbar.addEventListener("focus", function () {
+    this.closest(".combobox")
+      .querySelector(".combo-list")
+      .classList.add("activ");
+    this.style.width = "555px";
+    if (this.value.length >= 3) {
+      this.style.width = "185px";
     }
-  })
-})
+  });
+});
 
+document.addEventListener("click", function (e) {
+  document.querySelectorAll(".inputcombobox").forEach((searchbar) => {
+    if (e.target != searchbar && e.target.closest(".combobox") != searchbar.closest('.combobox')) {
+      searchbar.closest(".combobox")
+        .querySelector(".combo-list")
+        .classList.remove("activ");
+      searchbar.style.width = "185px";
+    }
+  });
+});
